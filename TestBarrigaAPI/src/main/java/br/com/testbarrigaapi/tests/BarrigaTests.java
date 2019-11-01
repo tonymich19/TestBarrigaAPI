2a875bd248d7d6e76600785039de4f0e87ab475c
Tony Correia <tony.correia19@outlook.com>
2019-11-01 03:40:55 -0300

    Adicionado cenários de teste: Adc e alterar conta com sucesso.

--- TestBarrigaAPI/src/main/java/br/com/testbarrigaapi/tests/BarrigaTests.java
+++ TestBarrigaAPI/src/main/java/br/com/testbarrigaapi/tests/BarrigaTests.java
@@ -2,11 +2,33 @@ package br.com.testbarrigaapi.tests;
 
 import static io.restassured.RestAssured.given;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import br.com.testbarrigaapi.BaseTest;
 
 public class BarrigaTests extends BaseTest{
+	private String TOKEN;
+	
+	@Before
+	public void login(){
+		Map<String, String> login = new HashMap<String, String>();
+		login.put("email", "tony_michael19@hotmail.com");
+		login.put("senha", "123456");
+		
+		TOKEN = given()
+			.body(login)
+		.when()
+			.post("/signin")
+		.then()
+			.statusCode(200)
+			.extract().path("token")
+		;
+	}
+	
 
 	@Test
 	public void naoDeveAcessarAPISemToken() {
@@ -14,7 +36,31 @@ public class BarrigaTests extends BaseTest{
 		.when()
 			.get("/contas")
 		.then()
-			.statusCode(401)
-		;	
+			.statusCode(401)		
+		;
+	}
+	@Test
+	public void AdicionarContaComSucesso() {
+		given()
+			.header("Authorization", "JWT " + TOKEN)
+			.body("{\"nome\": \"nova conta\"}")
+		.when()
+			.post("/contas")
+		.then()
+			.statusCode(201)
+		;
+	}
+	
+	@Test
+	public void deveAlterarContaComSucesso() {
+				
+		given()
+			.header("Authorization", "JWT " + TOKEN)
+			.body("{\"nome\": \"nova alteração\"}")
+		.when()
+			.put("/contas/38994")
+		.then()
+			.statusCode(200)
+		;
 	}
 }
